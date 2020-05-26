068e731227eac0f8ea24cf7a20844e7794e2a1f6
dewanganshivam89@gmail.com <dewanganshivam89@gmail.com>
2020-05-26 12:00:06 +0530

    [Shivam] Add. Rabbit mq listner to recieve message from queue

---
+++ ms-2/OrderConsumer.cs
@@ -0,0 +1,18 @@
+﻿using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ViewModel;
+
+namespace ms_2
+{
+    public class OrderConsumer : IConsumer<Order>
+    {
+        public async Task Consume(ConsumeContext<Order> context)
+        {
+            var data = context.Message;
+
+        }
+    }
+}
---
+++ ms-2/ViewModel/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public string ProductNAme { get; set; }
+        
+        public int Qty { get; set; }
+    }
+}
