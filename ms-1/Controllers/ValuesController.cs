37c0428fd9a0078cb15b809dc500bc1ea605ff5a
dewanganshivam89@gmail.com <dewanganshivam89@gmail.com>
2020-05-26 11:59:46 +0530

    [Shivam] Add. Rabbit mq Publishe ms-1r to publish message from queue

---
+++ ms-1/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MassTransit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ViewModel;
+
+namespace ms_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IBusControl _bus;
+
+        public ValuesController(IBusControl bus)
+        {
+            _bus = bus;
+        }
+
+        [HttpPost]
+       public async Task<IActionResult> CreateOrder(Order order)
+       {
+            Uri uri = new Uri("rabbitmq://localhost/order_queue");
+
+            var endpoint = await _bus.GetSendEndpoint(uri);
+            await endpoint.Send(order);
+
+            return Ok("Success");
+
+       }
+    }
+}
